88c3333cf730fa24c18c16d929f1073854a220f6
nobodyiam <nobodyiam@gmail.com>
2016-05-01 20:22:05 +0800

    update annotation

--- config-server/src/main/java/com/nobodyiam/spring/cloud/in/action/config/server/ConfigServerApplication.java
+++ config-server/src/main/java/com/nobodyiam/spring/cloud/in/action/config/server/ConfigServerApplication.java
@@ -1,16 +1,14 @@
 package com.nobodyiam.spring.cloud.in.action.config.server;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by Jason on 2/24/16.
  */
-@Configuration
-@EnableAutoConfiguration
 @EnableConfigServer
+@SpringBootApplication
 public class ConfigServerApplication {
 
   public static void main(String[] args) {
