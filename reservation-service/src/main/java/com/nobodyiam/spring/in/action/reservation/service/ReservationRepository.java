011ef0e51e7fa8d7327593d00432de244e9a0d52
nobodyiam <nobodyiam@gmail.com>
2016-05-05 21:30:54 +0800

    use spring data rest

--- reservation-service/src/main/java/com/nobodyiam/spring/in/action/reservation/service/ReservationRepository.java
+++ reservation-service/src/main/java/com/nobodyiam/spring/in/action/reservation/service/ReservationRepository.java
@@ -3,12 +3,17 @@ package com.nobodyiam.spring.in.action.reservation.service;
 import com.nobodyiam.spring.in.action.reservation.service.entity.Reservation;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
 
 /**
  * Created by Jason on 5/3/16.
  */
+@RepositoryRestResource
 public interface ReservationRepository extends PagingAndSortingRepository<Reservation, Long> {
-  List<Reservation> findByReservationName(String reservationName);
+  @RestResource(path = "by-name")
+  List<Reservation> findByReservationName(@Param("name") String reservationName);
 }
--- reservation-service/src/main/java/com/nobodyiam/spring/in/action/reservation/service/ReservationServiceApplication.java
+++ reservation-service/src/main/java/com/nobodyiam/spring/in/action/reservation/service/ReservationServiceApplication.java
@@ -1,8 +1,16 @@
 package com.nobodyiam.spring.in.action.reservation.service;
 
+import com.google.common.base.Splitter;
+
+import com.nobodyiam.spring.in.action.reservation.service.entity.Reservation;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 /**
  * Created by Jason on 5/3/16.
@@ -10,6 +18,17 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ReservationServiceApplication {
+  @Bean
+  CommandLineRunner runner(ReservationRepository rr) {
+    return args -> {
+      rr.deleteAll();
+
+      Splitter.on(",").omitEmptyStrings().trimResults().split("Tom, Jerry")
+              .forEach(x -> rr.save(new Reservation(x)));
+      rr.findAll().forEach(System.out::println);
+    };
+  }
+
   public static void main(String[] args) {
     new SpringApplicationBuilder(ReservationServiceApplication.class)
             .run(args);
